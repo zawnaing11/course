4f86697d6560a43b1c98265535902a323c17cecf
zawnaingtun-mbridge <zaw.naing.htun@frobom.com>
2024-02-24 14:47:01 +0630

    #7 authentication

---
+++ app/Http/Controllers/Admin/LoginController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\LoginRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function login()
+    {
+        return view('backend.auth.login');
+    }
+
+    public function authenticate(LoginRequest $request)
+    {
+        $validated = $request->validated();
+
+        if (Auth::attempt($validated)) {
+            $request->session()->regenerate();
+
+            return redirect()->route('dashboard');
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ])->onlyInput('email');
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect()->route('login');
+    }
+}
--- routes/backend.php
+++ routes/backend.php
@@ -2,9 +2,23 @@
 
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\CourseController;
+use App\Http\Controllers\Admin\LoginController;
 use Illuminate\Support\Facades\Route;
 
-//categories
-Route::resource('categories', CategoryController::class)->except('show');
-// course
-Route::resource('courses', CourseController::class)->except('show');
+// login
+Route::middleware('guest:web')->group(function () {
+    Route::get('login', [LoginController::class, 'login'])->name('login');
+    Route::post('login', [LoginController::class, 'authenticate'])->name('authenticate');
+});
+
+Route::middleware('auth')->group(function () {
+    // logout
+    Route::get('logout', [LoginController::class, 'logout'])->name('logout');
+    //dashboard
+    Route::view('/', 'backend.dashboard')->name('dashboard');
+    //categories
+    Route::resource('categories', CategoryController::class)->except('show');
+    // course
+    Route::resource('courses', CourseController::class)->except('show');
+});
+
