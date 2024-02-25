a573f2515d8deef68a4e7b41161b03f0ae4aff64
zawnaingtun-mbridge <zaw.naing.htun@frobom.com>
2024-02-25 18:09:49 +0630

    #16 api authentication

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\CourseController;
+use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,8 +17,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
 Route::apiResource('users', UserController::class);
+Route::post('login', [LoginController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::get('apply/course/{course}', [CourseController::class, 'applyCourse']);
+    Route::get('logout', [LoginController::class, 'logout']);
+});
