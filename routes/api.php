a6210e2d4effe536e2b93f03fda32ae21fa1ce76
zawnaingtun-mbridge <zaw.naing.htun@frobom.com>
2024-05-24 20:34:29 +0630

    fix route name duplicate

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::apiResource('users', UserController::class);
+Route::apiResource('users', UserController::class, ['names' => 'api.users']);
 Route::post('login', [LoginController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
